49ff0723395117b4d6d7d8cfde5d11968de62445
DemoiselleRenard <jorgehernandez@ubuntu.(none)>
2013-04-01 22:13:48 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Telecobros::Application.config.secret_token = '42c27ec1c6f752989a79e31c999de24d6541bb4acf9b490a27681b42f70b2fe33515556761d298789269d8cacedcf2a6518f3052eed158e1095c9bab6ea30479'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Telecobros::Application.config.session_store :cookie_store, key: '_Telecobros_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Telecobros::Application.config.session_store :active_record_store
